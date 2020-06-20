425b2818c3daa1966b8b53029d5fce1ac119d714
OptimalStrategy <17456182+OptimalStrategy@users.noreply.github.com>
2020-06-19 22:01:21 -0400

    switch to nightly rust; update song.ppga

--- scripts/song.ppga.lua
+++ scripts/song.ppga.lua
@@ -1,6 +1,12 @@
-local response, error = api:streamelements():song_requests():current_song()
-if error ~= nil then
+local se = api:streamelements()
+if se == nil then
+    util:error("StreamElements API is unavailable")
+    return ("StreamElements API is not configured")
+end
+
+local ok, err = api:streamelements():song_requests():current_song()
+if err ~= nil then
     return ("WAYTOODANK something broke")
 end
 
-return ("CheemJam now playing " .. tostring(response.title) .. " [ https://youtu.be/" .. tostring(response.videoId) .. " ]")
+return ("CheemJam now playing " .. tostring(ok.title) .. " [ https://youtu.be/" .. tostring(ok.videoId) .. " ]")
